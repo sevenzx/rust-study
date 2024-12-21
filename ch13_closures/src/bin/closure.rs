5b838a552bae065f1e0491a461b1dd4797d683a5
xuan <zx786@vip.qq.com>
2024-12-21 15:43:44 +0800

    feat: 闭包类型推断和标注

--- ch13_closures/src/bin/closure.rs
+++ ch13_closures/src/bin/closure.rs
@@ -18,6 +18,15 @@ fn generate_workout(intensity: u32, random_number: u32) {
     // 闭包的定义
     // 闭包的定义以一对竖线（|）开始，在竖线中指定闭包的参数；之所以选择这个语法是因为它与 Smalltalk 和 Ruby 的闭包定义类似。
     // 这个闭包有一个参数 num；如果有多于一个参数，可以使用逗号分隔，比如 |param1, param2|。
+
+    // 闭包不要求像 fn 函数那样在参数和返回值上注明类型。 闭包通常很短，并只关联于小范围的上下文而非任意情境。
+    // 在这些有限制的上下文中，编译器能可靠的推断参数和返回值的类型， 类似于它是如何能够推断大部分变量的类型一样。
+    // 类似于变量，如果相比严格的必要性你更希望增加明确性并变得更啰嗦，可以选择增加类型标注
+    // let expensive_closure = |num: u32| -> u32 {
+    //     println!("calculating slowly...");
+    //     thread::sleep(Duration::from_secs(2));
+    //     num
+    // };
     let expensive_closure = |num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
