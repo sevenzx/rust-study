d8afcbb1e9af1048dc342c9ea1d80a9697d12734
xuan <zx786@vip.qq.com>
2024-12-21 11:18:52 +0800

    feat: 处理环境变量实现大小写是否敏感

--- ch12_io_project_grep/src/lib.rs
+++ ch12_io_project_grep/src/lib.rs
@@ -1,21 +1,37 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
@@ -23,6 +39,7 @@ Pick three.";
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -32,7 +49,13 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        // is_err() 是 Result 类型的方法，用于检查结果是否为错误。如果环境变量未设置或无法访问，则 env::var 返回一个错误。
+        // is_err() 将返回 true。反之，如果环境变量存在且可访问，则返回 false
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        // 可以使用命令运行 CASE_INSENSITIVE=1 cargo run to poem.txt  就是大小写不敏感
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
@@ -46,10 +69,27 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results: Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
