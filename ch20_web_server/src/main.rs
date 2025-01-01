d47872f1163ba01ce0c414cbe56f92c1c046534d
xuan <zx786@vip.qq.com>
2025-01-01 23:03:21 +0800

    feat: async Web服务器

--- ch20_web_server/src/main.rs
+++ ch20_web_server/src/main.rs
@@ -1,44 +1,43 @@
+use async_std::net::TcpListener;
+use async_std::net::TcpStream;
+use async_std::prelude::*;
+use async_std::task;
+use futures::stream::StreamExt;
 use std::fs;
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream)
-    }
+#[async_std::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
+    listener
+        .incoming()
+        .for_each_concurrent(/* limit */ None, |tcpstream| async move {
+            let tcpstream = tcpstream.unwrap();
+            handle_connection(tcpstream).await;
+        })
+        .await;
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     // 开头增加 b"" 字节字符串语法将其转换为字节字符串
     let get = b"GET / HTTP/1.1\r\n";
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("./html/hello.html").unwrap();
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "./html/hello.html")
+    } else if buffer.starts_with(sleep) {
+        task::sleep(Duration::from_secs(5)).await;
+        ("HTTP/1.1 200 OK\r\n\r\n", "./html/hello.html")
     } else {
-        // 其他请求
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("./html/404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "./html/404.html")
+    };
 
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            status_line,
-            contents.len(),
-            contents
-        );
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{status_line}{contents}");
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
